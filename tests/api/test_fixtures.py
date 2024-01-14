ceee65ffa9fa033c63ec1c13fd04fe1e663e0a9d
Olena Korniichuk <elenakorneychuk.mail@gmail.com>
2024-01-14 07:58:18 +0200

    Changes in test_api.py

--- tests/api/test_fixtures.py
+++ tests/api/test_fixtures.py
@@ -1,29 +1,4 @@
-import pytest
 
-
-class User:
-
-    def __init__(self) -> None:
-        self.name = None
-        self.second_name = None
-
-    def create(self):
-        self.name = 'Sergii'
-        self.second_name = 'Butenko'
-
-    def remove(self):
-        self.name = ''
-        self.second_name = ''
-
-
-@pytest.fixture
-def user():
-    user = User()
-    user.create()
-    
-    yield user
-
-    user.remove()
 
 
 def test_change_name(user):
