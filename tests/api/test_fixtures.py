4a8580ef7e5f38076394493bbca8bc0c59a949a0
Olena Korniichuk <elenakorneychuk.mail@gmail.com>
2024-01-15 05:25:24 +0200

    Changes name, second_name

--- tests/api/test_fixtures.py
+++ tests/api/test_fixtures.py
@@ -4,11 +4,11 @@ import pytest
 
 @pytest.mark.check
 def test_change_name(user):
-    assert user.name == 'Sergii'
+    assert user.name == 'Olena'
 
 @pytest.mark.check
 def test_change_second_name(user):
-    assert user.second_name == 'Butenko'
+    assert user.second_name == 'Korniichuk'
 
 
 
