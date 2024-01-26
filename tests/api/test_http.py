46d15384b41aadc2f0ceb4f399f5daa3252094bc
Olena Korniichuk <elenakorneychuk.mail@gmail.com>
2024-01-26 07:53:30 +0200

    Changes unit_16.6.1 "Check_of_statuse_code_for_requirements_compliance"

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -11,7 +11,7 @@ def test_first_request():
 def test_second_request():
     r = requests.get('https://api.github.com/users/defunkt')
     print(f"Response Body is {r.json()}")
-    print(f"Response Status code is {r.status_code}")
+    assert r.status_code == 200
     print(f"Response Headers are {r.headers}")
 
 
