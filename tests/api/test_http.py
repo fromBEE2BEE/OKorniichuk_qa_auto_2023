2f9bed390a673d3eb935c9218180950eee1c8f54
Olena Korniichuk <elenakorneychuk.mail@gmail.com>
2024-01-26 08:15:55 +0200

    Changes unit_16.6.3 "Check_headers[Server]"

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -11,10 +11,11 @@ def test_first_request():
 def test_second_request():
     r = requests.get('https://api.github.com/users/defunkt')
     body = r.json()
+    headers = r.headers
 
     assert body['name'] == 'Chris Wanstrath'
     assert r.status_code == 200
-    print(f"Response Headers are {r.headers}")
+    assert headers['Server'] == 'GitHub.com'
 
 
 
