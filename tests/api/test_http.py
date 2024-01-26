5df8830b503affa101408052a6a1e0baf1a70ec9
Olena Korniichuk <elenakorneychuk.mail@gmail.com>
2024-01-26 06:23:35 +0200

    Changes unit_ 16.4

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -7,4 +7,11 @@ def test_first_request():
     r = requests.get('https://api.github.com/zen')
     print(f"Response is {r.text}")
 
+@pytest.mark.http
+def test_second_request():
+    r = requests.get('https://api.github.com/users/defunkt')
+    print(f"Response Body is {r.json()}")
+    print(f"Response Status code is {r.status_code}")
+    print(f"Response Headers are {r.headers}")
+
 
