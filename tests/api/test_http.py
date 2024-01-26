3b8a0cbca3e33f8ab56575f3ff22082934d30cd1
Olena Korniichuk <elenakorneychuk.mail@gmail.com>
2024-01-26 06:49:19 +0200

    Made corrections from 'Changes unit_16.4' to 'Changes unit_16.4.1'

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -5,13 +5,8 @@ import requests
 @pytest.mark.http
 def test_first_request():
     r = requests.get('https://api.github.com/zen')
-    print(f"Response is {r.text}")
+    print(r.text)
 
-@pytest.mark.http
-def test_second_request():
-    r = requests.get('https://api.github.com/users/defunkt')
-    print(f"Response Body is {r.json()}")
-    print(f"Response Status code is {r.status_code}")
-    print(f"Response Headers are {r.headers}")
+
 
 
