816b5570a049195c8dbce90e78bc5b6ab5ae0ab7
Olena Korniichuk <elenakorneychuk.mail@gmail.com>
2024-01-27 15:33:58 +0200

    Changes to API test No 17.3_1

--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -16,5 +16,6 @@ def test_user_not_exists(github_api):
 @pytest.mark.api
 def test_repo_can_be_found(github_api):
     r = github_api.search_repo('become-qa-auto')
-    print(r)
+    assert r['total_count'] == 13
+    assert 'become-qa-auto' in r['items'][0]['name']
     
