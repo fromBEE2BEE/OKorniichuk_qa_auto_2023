027415d579612edb806cc62affe788ed815e5419
Olena Korniichuk <elenakorneychuk.mail@gmail.com>
2024-02-14 19:54:53 +0200

    Changes.Correction of the failed test

--- tests/ui/test_ui.py
+++ tests/ui/test_ui.py
@@ -4,66 +4,77 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-import time
-
 
 @pytest.mark.ui
 def test_check_incorrect_username():
-    # Створення об'єкту для керування бразуером
+    # Create the object for browser management  
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-    # відкриваємо сторінку https://github.com/login
+    # Load the page: https://github.com/login 
     driver.get("https://github.com/login")
 
-    # Знаходимо поле, в яке будемо вводити неправильне ім'я користувача або поштову адресу
+    # Find the field for entering a wrong login or an email address 
     login_elem = driver.find_element(By.ID, "login_field")
 
-    # Вводимо неправильне ім'я користувача або поштову адрІесу
+    # Enter a wrong login or an email address 
     login_elem.send_keys("sergiibutenko@mistakeinemail.com")
 
-    # Знаходимо поле, в яке будемо вводити неправильний пароль
+    # Find the field for entering a wrong password      
     pass_elem = driver.find_element(By.ID, "password")
 
-    # Вводимо неправильний пароль
+    # Enter a wrong password 
     pass_elem.send_keys("wrong password")
 
-    # Знаходимо кнопку sign in
+    # Find the button "Sign in"
     btn_elem = driver.find_element(By.NAME, "commit")
 
-    # Емулюємо клік лівою кнопкою мишки
+    # Emulate a click of the left mouse button    
     btn_elem.click()
     
-    # Перевіряємо, що назва сторінки така, яку ми очікуємо
+    # Check that the page name where we are on is correct          
     assert driver.title == "Sign in to GitHub · GitHub"
 
-    # Закриваємо браузер
+    # Close the browser 
     driver.close()
 
 
 @pytest.mark.ui
 def test_search_cargo_positive():
-    # Створення об'єкту для керування бразуером
+    # Create the object for browser management 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-    # відкриваємо сторінку https://novaposhta.ua
+    # Load the  page: https://novaposhta.ua 
     driver.get("https://novaposhta.ua")
-       
-    # Знаходимо поле, в яке будемо вводити коректний номер накладної
+
+    # Find the proper field for entering the correct invoice number    
     cargo_number_elem = driver.find_element(By.ID, "cargo_number")
     
-    # Вводимо коректний номер накладної
+    # Fill in the correct invoice number 
     cargo_number_elem.send_keys('20450857971424')
     
-    # Знаходимо кнопку 'submit'
-    btn_elem = driver.find_element(By.CLASS_NAME, "search_cargo_form")
-    
-    # Емулюємо клік лівою кнопкою мишки
-    btn_elem.click()
-       
-    # Перевіряємо, що назва сторінки така, яку ми очікуємо
-    assert driver.title == 'Термінова і експрес доставка: транспортно-логістичні послуги в Києві та по всій Україні - служба доставки №1 «Нова пошта»'
+    # Find the button that closes an advertisement 
+    adv_btn = driver.find_element(By.XPATH, ".//div[@id='popup_info']//div[@class='header']//i[text()='закрыть']")
 
-    # Закриваємо браузер
+    # Emulate a click of the left mouse button
+    adv_btn.click()
+    
+    # Find the  'submit' button
+    btn_elem = driver.find_element(By.XPATH, ".//div[@id='wrapper']//input[@name='' and @type='submit']")
+   
+    # Emulate a click of the left mouse button
+    btn_elem.click()
+    
+    # Find the message with the text: 'Зрозуміло'
+    inform_elem = driver.find_element(By.XPATH, ".//button[@class='button v-btn v-btn--depressed v-btn--flat v-btn--outlined theme--light v-size--default']//span[@class='v-btn__content']")
+   
+    # Emulate a click of the left mouse button
+    inform_elem.click()
+   
+    # Check that the page name where we are on is correct  
+    assert driver.title == 'Трекінг Нова пошта - відстежити посилку, відслідковувати ТТН'
+    
+    # Close the browser
     driver.close()
+
 
 
